3570a5fc6763375279c876c0854d6904e80d2280
phmiranda <pehhagah.1607@gmail.com>
2019-05-16 16:15:13 -0300

    [update] atualização dos componentes iniciais do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavbarComponent } from './dashboard/navbar/navbar.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
+import { IndexComponent } from './dashboard/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+      LoginComponent,
+      NavbarComponent,
+      SidebarComponent,
+      FooterComponent,
+      IndexComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
