22759f0d9da59bd6bd1e3fa244d9582a039b5549
senaidf <contato@sistemafibra.com.br>
2019-06-13 21:16:57 -0300

    [add] - criação de novos componentes e funcionalidade para gerenciar produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,25 @@ import { LoginComponent } from './auth/login/login.component';
 
 /* comentário */
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
 
 /* comentário */
 import { UsuariosComponent } from './dashboard/seguranca/usuarios/usuarios.component';
 import { CategoriasComponent } from './dashboard/configuracao/categorias/categorias.component';
+import { ProdutosComponent } from './dashboard/administrativo/produtos/produtos.component';
 
 @NgModule({
   declarations: [
       LoginComponent,
       NavbarComponent,
+      SidebarComponent,
+      FooterComponent,
+      PrincipalComponent,
       UsuariosComponent,
       CategoriasComponent,
+      ProdutosComponent,
   ],
   imports: [
     BrowserModule,
